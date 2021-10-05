2b9cd9da7c2687843ddaa6519c4c4aee85c07355
Claudio_Casellato <claudiocasellatoml@gmail.com>
2021-10-05 20:31:46 +0200

    test_file rm

--- src/test_file.py
+++ src/test_file.py
@@ -1,56 +1,37 @@
 import tensorflow as tf
 from tensorflow.keras import backend as k
+from tensorflow.keras import initializers
 from tensorflow.keras import layers as l
+from tensorflow.keras import activations as a
 from tensorflow.keras import models as m
 import numpy as np
 
 
-class GraphLayer(l.Layer):
+class BaseGraph(l.Layer):
     def __init__(self, **kwargs):
-        super(GraphLayer, self).__init__(**kwargs)
+        super(BaseGraph, self).__init__(**kwargs)
 
     def build(self, input_shape):
         print(input_shape)
 
     def call(self, inputs, *args, **kwargs):
-        print(inputs)
-        print(args)
-        print(kwargs)
-        return inputs
+        edgelist = inputs[0]
+        s_embedding = inputs[1]
+        t_embedding = inputs[2]
+        r_embedding = inputs[3]
+        values = inputs[4]
+
+        message_s = self.aggregate(s_embedding, edgelist[:, 1])
+        message_t = self.aggregate(r_embedding, edgelist[:2])
+        t_propagate = self.propagate(message_s, edgelist[:, 0])
+        return t_propagate
+
+    def aggregate(self, inputs):
+        raise NotImplemented
+
+    def propagate(self, embeddings, indices):
+        raise NotImplemented
 
 
-def make_data(years, relations, nodes):
-    tmp_adj_list = []
-    for t in range(years):
-        for r in range(relations):
-            for importer in range(nodes):
-                for exporter in range(nodes):
-                    if importer!=exporter:
-                        link = np.random.choice((0,1))
-                        if link:
-                            value = np.random.lognormal(0,1)
-                            tmp_adj_list.append((t, r, importer, exporter, value))
-    tmp_adj_list = np.asarray(tmp_adj_list)
-    sparse = tf.sparse.SparseTensor(tmp_adj_list[:,:-1], tmp_adj_list[:,-1], (years, relations, nodes, nodes))
-    return sparse
 
 
-if __name__ == '__main__':
-    layer = GraphLayer()
-    nodes = 150
-    ft = 10
-    t = 20
-    rel = 5
-    t = 60
-    '''input_shape1 = (t, nodes, nodes, rel)
-    input_shape2 = (t, nodes, ft, rel)
-    i1 = l.Input(input_shape1, name="features")
-    i2 = l.Input(input_shape2, name="realtions")
-    o1 = layer([i1, i2])
-    model = m.Model({i1, i2}, o1)
-    features = np.random.normal(0,1,input_shape1)
-    relations = np.random.normal(0,1,input_shape2)
-    input = {features, relations}
-    preds = model.predict(input)
-    '''
-    #data = make_data(t, rel, nodes)
