fixed gradients ntn

src/modules/bilinear.py
@@ -4,12 +4,12 @@ import tensorflow.keras.backend as k
 import numpy as np
 from tensorflow.keras.losses import mean_squared_error, sparse_categorical_crossentropy
 
-physical_devices = tf.config.list_physical_devices('CPU')
-#tf.config.experimental.set_memory_growth(physical_devices[0], True)
+physical_devices = tf.config.list_physical_devices("CPU")
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 class BilinearLayer(keras.layers.Layer):
-    """"
+    """ "
     Implementation of Bilinear Layer:
     formula for single vector -> bilinear_r(e1, e2) = e^t*R_r*e where e:R^f and R_r: R^(fxf)
     formula for matrix -> ExRxE^T where E:R^(Nxf) and R: R^(fxf)
@@ -30,7 +30,12 @@ class BilinearLayer(keras.layers.Layer):
             # for each relation add trainable weight matrix of size fxf
             for i in range(self.relations):
                 self.Bs.append(
-                    self.add_weight(shape=(self.features, self.features), trainable=True, name=f"Relation{i}"))
+                    self.add_weight(
+                        shape=(self.features, self.features),
+                        trainable=True,
+                        name=f"Relation{i}",
+                    )
+                )
 
     def call(self, inputs, *args, **kwargs):
         result = []
@@ -60,13 +65,17 @@ class idx_to_Features(keras.layers.Layer):
     def build(self, input_shape):
         self.layers = []
         for i in range(self.n_relations):
-            self.layers.append(keras.layers.Dense(units=self.n_output, activation="relu"))
+            self.layers.append(
+                keras.layers.Dense(units=self.n_output, activation="relu")
+            )
 
     def call(self, inputs, **kwargs):
         output = []
         for i in range(self.n_relations):
             output.append(self.layers[i](inputs))
-        return tf.transpose(output, perm=[1, 0, 2, 3])  # batches x relations x nodes x features
+        return tf.transpose(
+            output, perm=[1, 0, 2, 3]
+        )  # batches x relations x nodes x features
 
 
 if __name__ == "__main__":
@@ -74,13 +83,18 @@ if __name__ == "__main__":
     nodes = 20
     relations = 5
     features = 4
-    nodes_idx_to_onehot = np.zeros(shape=(batches, nodes, nodes))  # batches x nodes x features
+    nodes_idx_to_onehot = np.zeros(
+        shape=(batches, nodes, nodes)
+    )  # batches x nodes x features
     idxs = np.arange(nodes)
-    nodes_idx_to_onehot[:, idxs, idxs] = 1.
+    nodes_idx_to_onehot[:, idxs, idxs] = 1.0
 
-    np_input = np.ones(shape=(batches, relations, nodes, features))  # (batch, relations, nodes, features)
-    adj = np.random.choice([0, 1], batches * nodes * nodes * relations, p=[0.5, 0.5]).reshape(batches, relations, nodes,
-                                                                                              nodes)
+    np_input = np.ones(
+        shape=(batches, relations, nodes, features)
+    )  # (batch, relations, nodes, features)
+    adj = np.random.choice(
+        [0, 1], batches * nodes * nodes * relations, p=[0.5, 0.5]
+    ).reshape(batches, relations, nodes, nodes)
 
     # input_layer = keras.layers.InputLayer(input_shape=np_input.shape[1:])
     input_layer = keras.layers.Input(shape=nodes_idx_to_onehot.shape[1:])
@@ -90,4 +104,4 @@ if __name__ == "__main__":
     model.compile(optimizer="rmsprop", loss=mean_squared_error)
     model.fit(nodes_idx_to_onehot, adj, epochs=20)
     preds = model.predict(nodes_idx_to_onehot)
-    print(preds[0,0,:,:])
+    print(preds[0, 0, :, :])

src/modules/graph_utils.py
@@ -21,24 +21,45 @@ def sample_zero_edges(A, p):
     edge_set = set()
     for e in edgelist:
         edge_set.add(tuple(e))
-    i = np.unique(edges[:, 0]).tolist()
-    j = np.unique(edges[:, 1]).tolist()
-    indices = set()
-    for idx in i:
-        indices.add(idx)
-    for idx in j:
-        indices.add(idx)
-    indices = list(indices)
-    mesh = np.meshgrid(indices, indices)
-    p1 = mesh[0].reshape(-1)
-    p2 = mesh[1].reshape(-1)
-    tot_edges = np.concatenate([p1[:, np.newaxis], p2[:, np.newaxis]], 1)
-    zero_edges = []
-    for edge in tot_edges:
-        if tuple(edge) not in edge_set:
-            if np.random.uniform(0, 1) < p:
-                zero_edges.append(edge)
-    sparse_zero = tf.sparse.SparseTensor(zero_edges, np.zeros(len(zero_edges), dtype=np.float32), A.shape)
+    if len(A.shape) == 2:
+        # i = set(np.unique(edges[:, 0]).tolist())
+        # j = set(np.unique(edges[:, 1]).tolist())
+        # indices = i.union(j)
+        # indices = list(indices)
+        indices = np.arange(A.shape[-1])
+        mesh = np.meshgrid(indices, indices)
+        p1 = mesh[0].reshape(-1)
+        p2 = mesh[1].reshape(-1)
+        tot_edges = np.concatenate([p1[:, np.newaxis], p2[:, np.newaxis]], 1)
+    elif len(A.shape) == 3:
+        i = set(np.unique(edges[:, 1]).tolist())
+        j = set(np.unique(edges[:, 2]).tolist())
+        relations = np.unique(edges[:, 0]).tolist()
+        indices = i.union(j)
+        indices = list(indices)
+        mesh = np.meshgrid(relations, indices, indices)
+        p1 = mesh[0].reshape(-1)
+        p2 = mesh[1].reshape(-1)
+        p3 = mesh[2].reshape(-1)
+        tot_edges = np.concatenate(
+            [p1[:, np.newaxis], p2[:, np.newaxis], p3[:, np.newaxis]], 1
+        )
+    tot_edges = np.unique(tot_edges, axis=0)
+    tot_edges = [tuple(a) for a in tot_edges]
+    tot_edges = set(tot_edges)
+    non_zero_edges = np.unique(A.indices.numpy(), axis=0)
+    non_zero_edges = [tuple(a) for a in non_zero_edges]
+    non_zero_edges = set(non_zero_edges)
+    allowed_edges = tot_edges - non_zero_edges
+    allowed_edges = np.asarray(list(allowed_edges))
+    allowed_edges_idx = np.arange(len(allowed_edges))
+    sampled_edges = np.random.choice(
+        allowed_edges_idx, replace=False, size=int(len(allowed_edges) * p)
+    )
+    sampled_edges = allowed_edges[sampled_edges]
+    sparse_zero = tf.sparse.SparseTensor(
+        sampled_edges, np.zeros(len(sampled_edges), dtype=np.float32), A.shape
+    )
     return tf.sparse.reorder(tf.sparse.add(A, sparse_zero))
 
 
@@ -50,6 +71,46 @@ def mask_sparse(A_sparse, p=0.5):
     A0_values = A0_values[mask]
     A0 = tf.sparse.SparseTensor(A0_ind, A0_values, np.max(A0_ind, 0))
     return A0, mask
+
+
+def add_self_loops_data(data_sp: tf.sparse.SparseTensor):
+    n_nodes = data_sp.shape[-1]
+    years = data_sp.shape[0]
+    r = data_sp.shape[1]
+    self_edges = []
+    self_values = []
+    for t in range(years):
+        data_t_dense = tf.sparse.to_dense(
+            tf.sparse.slice(data_sp, (t, 0, 0, 0), (1, r, n_nodes, n_nodes))
+        )[0]
+        deg_out = tf.reduce_sum(data_t_dense, axis=2)
+        deg_sparse = tf.sparse.from_dense(deg_out)
+        self_edges_t = np.asarray(deg_sparse.indices)
+        idx = self_edges_t[:, -1]
+        self_edges_t = np.concatenate(
+            [
+                np.ones(len(idx), dtype=np.int64)[:, np.newaxis] * t,
+                self_edges_t,
+                idx[:, np.newaxis],
+            ],
+            axis=1,
+        )
+        self_values_t = deg_sparse.values
+        self_edges.extend(self_edges_t.tolist())
+        self_values.extend(self_values_t)
+    self_data_sp = tf.sparse.SparseTensor(self_edges, self_values, data_sp.shape)
+    data_sp = tf.sparse.add(data_sp, self_data_sp)
+    data_sp = tf.sparse.reorder(data_sp)
+    return data_sp
+
+
+def add_self_loop(data: tf.sparse.SparseTensor):
+    i = np.arange(data.shape[-1])
+    edges = np.concatenate([i[:, None], i[:, None]], -1)
+    values = tf.math.unsorted_segment_sum(data.values, data.indices[:, 0], data.shape[-1])
+    self_sparse = tf.sparse.SparseTensor(edges, values, data.shape)
+    return tf.sparse.add(data, self_sparse)
+
 
 def make_data(years, relations, nodes):
     tmp_adj_list = []
@@ -64,6 +125,36 @@ def make_data(years, relations, nodes):
                             # value = np.cast["float32"](value)
                             tmp_adj_list.append((t, r, importer, exporter, value))
     tmp_adj_list = np.asarray(tmp_adj_list, dtype=np.float32)
-    sparse = tf.sparse.SparseTensor(tmp_adj_list[:, :-1], tmp_adj_list[:, -1], (years, relations, nodes, nodes))
+    sparse = tf.sparse.SparseTensor(
+        tmp_adj_list[:, :-1], tmp_adj_list[:, -1], (years, relations, nodes, nodes)
+    )
     return sparse
 
+
+def predict_all_sparse(A_true):
+    if len(A_true.shape) == 2:
+        # A_true = tf.sparse.to_dense(tf.sparse.reorder(A_true))
+        i = np.arange(A_true.shape[-1])
+        i, j = np.meshgrid(i, i)
+        i = i.reshape(-1)[:, None]
+        j = j.reshape(-1)[:, None]
+        tot_edges = np.concatenate([i, j], -1)
+        return tf.sparse.reorder(
+            tf.sparse.SparseTensor(
+                tot_edges, np.random.normal(size=len(tot_edges)), A_true.shape
+            )
+        )
+    elif len(A_true.shape) == 3:
+        # A_true = tf.sparse.to_dense(tf.sparse.reorder(A_true))
+        i = np.arange(A_true.shape[-1])
+        k = np.arange(A_true.shape[0])
+        k, i, j = np.meshgrid(k, i, i)
+        i = i.reshape(-1)[:, None]
+        j = j.reshape(-1)[:, None]
+        k = k.reshape(-1)[:, None]
+        tot_edges = np.concatenate([k, i, j], -1)
+        return tf.sparse.reorder(
+            tf.sparse.SparseTensor(
+                tot_edges, np.random.normal(size=len(tot_edges)), A_true.shape
+            )
+        )

src/modules/losses.py
@@ -4,15 +4,16 @@ import tensorflow.keras.layers as l
 
 @tf.function
 def square_loss(y_true, y_pred):
-    if isinstance(y_true, tf.sparse.SparseTensor) and isinstance(y_pred, tf.sparse.SparseTensor):
+    if isinstance(y_true, tf.sparse.SparseTensor) and isinstance(
+        y_pred, tf.sparse.SparseTensor
+    ):
         diff = tf.square(y_true.values - y_pred.values)
-        return tf.reduce_sum(diff)
+        return tf.sqrt(tf.reduce_sum(diff))
 
     if isinstance(y_true, tf.Tensor) and isinstance(y_pred, tf.Tensor):
         diff = tf.square(y_true - y_pred)
-        print(f"DIFF SHAPE {diff.shape}")
         diff = tf.reshape(diff, [-1])
-        return tf.reduce_sum(diff)
+        return tf.sqrt(tf.reduce_sum(diff))
 
     if isinstance(y_true, tf.sparse.SparseTensor) and isinstance(y_pred, tf.Tensor):
         y_true = tf.sparse.to_dense(y_true)
@@ -21,13 +22,20 @@ def square_loss(y_true, y_pred):
         diff = tf.reshape(diff, [-1])
         return tf.reduce_sum(diff)
 
-    raise Exception(f"Inputs and Predictions are of different types: {type(y_true)}, {type(y_pred)}")
+    raise Exception(
+        f"Inputs and Predictions are of different types: {type(y_true)}, {type(y_pred)}"
+    )
 
 
 @tf.function
 def log_normal_likelihood_loss(y_true, mu, sigma):
-    likelihood = - (tf.reduce_sum(tf.math.square(tf.math.log(y_true.values) - mu) / (2 * tf.math.square(sigma)))
-                    + tf.reduce_sum(tf.math.log(sigma)))
+    likelihood = -(
+        tf.reduce_sum(
+            tf.math.square(tf.math.log(y_true.values) - mu)
+            / (2 * tf.math.square(sigma))
+        )
+        + tf.reduce_sum(tf.math.log(sigma))
+    )
     nll = -likelihood
     print(f"loss shape: {nll.shape}")
     return nll
@@ -72,6 +80,6 @@ class EmbeddingSmoothnessRegularizer(l.Layer):
         self.rate = rate
 
     def call(self, inputs, **kwargs):
-        loss = self.rate*embedding_smoothness(*inputs)
+        loss = self.rate * embedding_smoothness(*inputs)
         self.add_loss(loss)
         return loss