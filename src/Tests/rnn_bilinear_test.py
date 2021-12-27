36d4a5b22ff678cb191d86c6ad857ed5391ded8c
Claudio_Casellato <claudiocasellatoml@gmail.com>
2021-12-27 20:05:07 +0100

    rnn_bil and gat_bil update

--- src/Tests/rnn_bilinear_test.py
+++ src/Tests/rnn_bilinear_test.py
@@ -8,12 +8,13 @@ import os
 import pickle as pkl
 from src.modules.layers import BatchBilinearDecoderDense
 from spektral.layers.convolutional import GATConv
+import matplotlib.animation as animation
 
 
 class RnnBil(k.models.Model):
     def __init__(self,
                  gat_channels=5,
-                 gat_heads=5,
+                 gat_heads=15,
                  rnn_units=25,
                  activation="relu",
                  dropout_rate=0.5,
@@ -27,7 +28,7 @@ class RnnBil(k.models.Model):
         self.residual_con = residual_con
         self.initial = True
 
-        self.rnn_lower = k.layers.SimpleRNN(
+        self.rnn = k.layers.SimpleRNN(
             units=self.rnn_units,
             activation=self.activation,
             dropout=self.dropout_rate,
@@ -35,22 +36,8 @@ class RnnBil(k.models.Model):
             return_sequences=True,
             time_major=True
         )
-        self.rnn_upper = k.layers.SimpleRNN(
-            units=self.rnn_units,
-            activation=self.activation,
-            dropout=self.dropout_rate,
-            recurrent_dropout=self.dropout_rate,
-            return_sequences=True,
-            time_major=True
-        )
-        self.encoder_lower = GATConv(
-            channels=self.gat_channels,
-            attn_heads=self.gat_heads,
-            dropout_rate=self.dropout_rate,
-            activation=self.activation,
-            concat_heads=True,
-        )
-        self.encoder_upper = GATConv(
+
+        self.encoder = GATConv(
             channels=self.gat_channels,
             attn_heads=self.gat_heads,
             dropout_rate=self.dropout_rate,
@@ -61,10 +48,8 @@ class RnnBil(k.models.Model):
             activation=self.activation,
             qr=True
         )
-        self.ln_enc_lower = k.layers.LayerNormalization()
-        self.ln_enc_upper = k.layers.LayerNormalization()
-        self.ln_rnn_lower = k.layers.LayerNormalization()
-        self.ln_rnn_upper = k.layers.LayerNormalization()
+        self.ln_enc = k.layers.LayerNormalization()
+        self.ln_rnn = k.layers.LayerNormalization()
 
     def call(self, inputs, training=None, mask=None):
         """
@@ -73,30 +58,13 @@ class RnnBil(k.models.Model):
             - A: shape(TxNxN)
         """
         x, a = inputs
-        a_lower = tf.linalg.LinearOperatorLowerTriangular(a).to_dense()
-        a_lower = a_lower + tf.transpose(a_lower)
-        a_upper = tf.linalg.LinearOperatorLowerTriangular(tf.transpose(a)).to_dense()
-        a_upper = a_upper + tf.transpose(a_upper)
         # encode and normalize
-        x_enc_lower = self.encoder_lower([x, a_lower])
-        x_enc_lower = self.ln_enc_lower(x_enc_lower)
-        x_enc_upper = self.encoder_upper([x, a_upper])
-        x_enc_upper = self.ln_enc_upper(x_enc_upper)
-        x_enc = tf.concat([x_enc_lower, x_enc_upper])
+        x_enc = self.encoder([x, a])
+        x_enc_lower = self.ln_enc(x_enc)
 
         # recurrent connection and normalize
-        if self.initial:
-            initial_state = np.zeros(shape=(x.shape[1], self.rnn_units))
-            x_rec_lower = self.rnn_lower(x_enc_lower, initial_state=initial_state)
-            x_rec_upper = self.rnn_lower(x_enc_upper, initial_state=initial_state)
-            self.initial = False
-        else:
-            x_rec_lower = self.rnn_lower(x_enc_lower)
-            x_rec_upper = self.rnn_lower(x_enc_upper)
-
-        x_rec_lower = self.ln_rnn_lower(x_rec_lower)
-        x_rec_upper = self.ln_rnn_upper(x_rec_upper)
-        x_rec = tf.concat([x_rec_lower, x_rec_upper], -1)
+        x_rec = self.rnn(x_enc_lower)
+        x_rec = self.ln_rnn(x_rec)
         if self.residual_con:
             x_rec = tf.concat([x_enc, x_rec], -1)
         _, A = self.decoder([x_rec, a])
@@ -105,18 +73,18 @@ class RnnBil(k.models.Model):
 
 class GatBil(k.models.Model):
     def __init__(self,
-                 gat_channels=10,
-                 gat_heads=10,
-                 rnn_units=50,
+                 gat_channels=5,
+                 gat_heads=15,
                  activation="relu",
                  dropout_rate=0.5,
+                 lag=5,
                  residual_con=True):
         super(GatBil, self).__init__()
         self.gat_channels = gat_channels
         self.gat_heads = gat_heads
-        self.rnn_units = rnn_units
         self.dropout_rate = dropout_rate
         self.activation = activation
+        self.lag = lag
         self.residual_con = residual_con
 
         self.rnn = GATConv(
@@ -137,6 +105,8 @@ class GatBil(k.models.Model):
             activation=self.activation,
             qr=False
         )
+        self.ln_enc = k.layers.LayerNormalization(axis=-1)
+        self.ln_rec = k.layers.LayerNormalization(axis=-1)
 
     @staticmethod
     def generate_list_lower_triang(batch, t, lag):
@@ -154,10 +124,12 @@ class GatBil(k.models.Model):
         """
         x, a = inputs
         t, n, _ = a.shape
-        a_mask = self.generate_list_lower_triang(n, t, 4)  # NxTxT
+        a_mask = self.generate_list_lower_triang(n, t, self.lag)  # NxTxT
         x_enc = self.encoder([x, a])
+        x_enc = self.ln_enc(x_enc)
         x_enc_t = tf.transpose(x_enc, (1, 0, 2))  # NxTxd
         x_rec = self.rnn([x_enc_t, a_mask])
+        x_rec = self.ln_rec(x_rec)
         x_rec = tf.transpose(x_rec, (1, 0, 2))  # TxNxd
         if self.residual_con:
             x_rec = tf.concat([x_enc, x_rec], -1)
@@ -176,6 +148,7 @@ if __name__ == "__main__":
     parser.add_argument("--n", type=int, default=174)
     parser.add_argument("--batches", default=2)
     parser.add_argument("--epochs", default=120)
+    parser.add_argument("--train", type=float, default=0.8)
     parser.add_argument("--sparse", action="store_true")
     args = parser.parse_args()
     synthetic = args.synthetic
@@ -184,6 +157,7 @@ if __name__ == "__main__":
     sparse = args.sparse
     batches = args.batches
     epochs = args.epochs
+    train_perc = args.train
     if synthetic:
         input_shape_nodes = (t, n, n)
         input_shape_adj = (t, n, n)
@@ -216,28 +190,59 @@ if __name__ == "__main__":
     optimizer = k.optimizers.Adam(0.001)
     loss_hist = []
     epochs = 200
-    #model = RnnBil()
-    model = GatBil()
+
+    # Train & Test Edge Masking
+    train_mask = np.random.choice((0, 1), p=(1-train_perc, train_perc), size=(t, n, n))
+    test_mask = 1 - train_mask
+    At_train_in = At[:-1] * train_mask[:-1]
+    At_train_out = At[1:] * train_mask[1:]
+    At_test_in = At[:-1] * test_mask[:-1]
+    At_test_out = At[1:] * test_mask[1:]
+
+    model = RnnBil()
+    #model = GatBil(lag=10, residual_con=False, dropout_rate=0.1)
     bar = tqdm.tqdm(total=epochs, leave=True)
     for i in range(epochs):
         with tf.GradientTape() as tape:
-            x_, a_ = model([Xt[:-1], At[:-1]])
-            At_flat = tf.reshape(At[1:], [-1])
-            #a_mask = tf.where(At_flat > 0, 1.0, 0.0)
-            a_flat = tf.reshape(a_, [-1])
-            #a_flat_masked = a_flat * a_mask
-            loss = k.losses.mean_squared_error(At_flat, a_flat)
-            loss_hist.append(loss)
-            grads = tape.gradient(loss, model.trainable_weights)
+            x_train, a_train = model([Xt[:-1], At_train_in])
+            a_train = a_train * train_mask[1:]
+            x_test, a_test = model([Xt[:-1], At_test_in])
+            a_test = a_test * test_mask[1:]
+            At_flat_train = tf.reshape(At_train_out, [-1])
+            a_flat_train = tf.reshape(a_train, [-1])
+            At_flat_test = tf.reshape(At_test_out, [-1])
+            a_flat_test = tf.reshape(a_test, [-1])
+            loss_train = k.losses.mean_squared_error(At_flat_train, a_flat_train)
+            loss_test = k.losses.mean_squared_error(At_flat_test, a_flat_test)
+            loss_hist.append((loss_train, loss_test))
+            grads = tape.gradient(loss_train, model.trainable_weights)
             optimizer.apply_gradients(zip(grads, model.trainable_weights))
         bar.update(1)
-    plt.plot(loss_hist)
-    fig, ax = plt.subplots(2, 1)
-    t = ax[0].imshow(At[-1].numpy())
-    ax[0].set_title("True")
-    fig.colorbar(t, ax=ax[0], fraction=0.05)
-    p = ax[1].imshow(a_[-1])
-    ax[1].set_title("Pred")
-    fig.colorbar(p, ax=ax[1], fraction=0.05)
 
+    losses = plt.plot(loss_hist)
+    plt.legend(iter(losses), ("Train", "Test"))
+    fig, ax = plt.subplots(1, 2)
+
+
+    def update(i):
+        ax[0].clear()
+        ax[1].clear()
+        fig.suptitle(f"Year {10 + i}")
+        ax[0].set_title("Pred")
+        t = ax[0].imshow(a_train[i], animated=True)
+        ax[1].set_title("True")
+        p = ax[1].imshow(At_train_out[i], animated=True)
+        # fig.colorbar(t, ax=ax[0], fraction=0.05)
+        # fig.colorbar(p, ax=ax[1], fraction=0.05)
+
+
+    anim = animation.FuncAnimation(
+        fig, update, frames=len(a_train) - 1, repeat=True
+    )
+    '''
+    writer = animation.FFMpegWriter(50)
+    if save:
+        anim.save(os.path.join(path, "animation.mp4"), writer=writer)
+        print("mp4 saved to {}".format(os.path.join(path, "animation.mp4")))
+    '''
     plt.show()
