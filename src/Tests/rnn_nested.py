993dd8248bd85b6b0f1ae0b92cf7776417afed17
Claudio_Casellato <claudiocasellatoml@gmail.com>
2022-03-01 01:12:38 +0100

    nested rnn lognormal works

--- src/Tests/rnn_nested.py
+++ src/Tests/rnn_nested.py
@@ -12,10 +12,11 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import argparse
+from src.modules.utils import compute_sigma
 
 
 class SimpleRnn(DropoutRNNCellMixin, l.Layer):
-    def __init__(self, units=10, activation="relu", dropout=0.1, recurrent_dropout=0.1, **kwargs):
+    def __init__(self, units=10, activation="relu", dropout=0.2, recurrent_dropout=0.2, **kwargs):
         super(SimpleRnn, self).__init__(**kwargs)
         self.units = units
         self.activation = activation
@@ -48,9 +49,9 @@ class SimpleRnn(DropoutRNNCellMixin, l.Layer):
 
     def get_config(self):
         config = {'dropout': self.dropout,
-                'recurrent_dropout': self.recurrent_dropout,
-                'units': self.units,
-                'activation': self.activation}
+                  'recurrent_dropout': self.recurrent_dropout,
+                  'units': self.units,
+                  'activation': self.activation}
         config.update(_config_for_enable_caching_device(self))
         base_config = super(SimpleRnn, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
@@ -60,103 +61,183 @@ if __name__ == "__main__":
     os.chdir("../../")
     parser = argparse.ArgumentParser()
     parser.add_argument("--k", action="store_true")
+    parser.add_argument("--gat", action="store_true")
+    parser.add_argument("--stateful", action="store_true")
+    parser.add_argument("--dropout", type=float, default=0.2)
+    parser.add_argument("--logits", action="store_true")
+    parser.add_argument("--seq_len", type=int, default=6)
     args = parser.parse_args()
     keras = args.k
-    '''
-    a, x, N, t = utils.load_dataset()
-    f = N
-    a = tf.transpose(a, perm=(1, 0, 2, 3))
-    x = tf.transpose(x, perm=(1, 0, 2, 3))
-    #cell = models.NestedCell(N, f, 5, 5, 10)
-    #cell = models.NestedCell2(N, f, 5, 5, 10)
-    cell = models.NestedCell3(N, f, 5, 5, 10)
-    rnn = tf.keras.layers.RNN(cell, return_sequences=True, return_state=False)
-    input_1 = tf.keras.Input((None, N, f))
-    input_2 = tf.keras.Input((None, N, N))
-    outputs = rnn((input_1, input_2))
-    model = tf.keras.models.Model([input_1, input_2], outputs)
-    o = model([x, a], training=False)
-    # print(o.shape)
-    model.compile(optimizer="Adam",
-                  loss=lambda true,pred : tf.reduce_mean(tf.math.square(true-pred), (-1, -2, -3)))
-    history = model.fit([a, x], epochs=100)
-    o = model([x, a])
-    history = history.history["loss"]
-    
-    history = []
-    epochs = 50
-    optimizer = tf.keras.optimizers.Adam(0.001)
-    for e in range(epochs):
-        with tf.GradientTape(persistent=True) as tape:
+    gat = args.gat
+    stateful = args.stateful
+    dropout = args.dropout
+    logits = args.logits
+    seq_len = args.seq_len
+
+    if gat:
+        a, x, N, t = utils.load_dataset(baci=True)
+        f = x.shape[-1]
+        a = tf.transpose(a, perm=(1, 0, 2, 3))  # BxTxNxN
+        x = tf.transpose(x, perm=(1, 0, 2, 3))  # # BxTxNxf
+        input_1 = tf.keras.Input((None, N, f))
+        input_2 = tf.keras.Input((None, N, N))
+        if logits:
+            if stateful:
+                batches_a = np.empty(shape=(1, seq_len, N, N))
+                batches_x = np.empty(shape=(1, seq_len, N, N))
+                for i in range(0, t-seq_len, seq_len):
+                    batches_a = np.concatenate([batches_a, a[:1, i:i + seq_len, :]], axis=0)
+                    batches_x = np.concatenate([batches_x, x[:1, i:i + seq_len, :]], axis=0)
+                a = np.concatenate([batches_a, a[:1, t-seq_len:, :]], axis=0)[1:]
+                x = np.concatenate([batches_x, x[:1, t-seq_len:, :]], axis=0)[1:]
+                input_1 = tf.keras.Input((None, N, f), batch_size=a.shape[0])
+                input_2 = tf.keras.Input((None, N, N), batch_size=a.shape[0])
+
+            cell = models.RecurrentEncoderDecoder(nodes=N,
+                                                  features=f,
+                                                  channels=5,
+                                                  attention_heads=8,
+                                                  hidden_size=25,
+                                                  dropout=dropout,
+                                                  recurrent_dropout=dropout)
+            rnn = tf.keras.layers.RNN(cell, return_sequences=True, return_state=False, stateful=stateful)
+            outputs = rnn((input_1, input_2))
+            model = tf.keras.models.Model([input_1, input_2], outputs)
+            model.compile(optimizer="rmsprop",
+                          loss=lambda true, logits:
+                          tf.reduce_sum(
+                              losses.zero_inflated_lognormal_loss(tf.expand_dims(true, -1),
+                                                                  logits,
+                                                                  reduce_axis=(-1, -2, -3)
+                                                                  ),
+                              axis=1)
+                          )
+            history = model.fit(x=[x[:, :-1, :], a[:, :-1, :]], y=a[:, 1:, :], epochs=150, verbose=1)
             o = model([x, a])
-            loss = losses.zero_inflated_lognormal_loss(tf.expand_dims(a, -1), o)
-            history.append(loss)
-            print(loss.numpy())
-        gradients = tape.gradient(loss, model.trainable_weights)
-        optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+            o = tf.reshape(o, (-1, N, N, 3))
+            history = history.history["loss"]
+            plt.plot(history)
+            ln = utils.zero_inflated_lognormal(o)
+            samples = ln.sample(1)
+            samples = tf.squeeze(samples)
+            samples = tf.clip_by_value(tf.math.log(samples), 0, 1e10)
+            a = tf.reshape(a, (-1, N, N))
+            a = tf.clip_by_value(tf.math.log(a), 0, 1e10)
 
-    plt.plot(history)
-    ln = utils.zero_inflated_lognormal(o)
-    samples = ln.sample(1)
-    samples = tf.squeeze(samples)
-    samples = tf.clip_by_value(tf.math.log(samples), 0,1e10)
-    a = tf.squeeze(a)
-    a = tf.clip_by_value(tf.math.log(a), 0, 1e10)
-    fig, axs = plt.subplots(1,2)
-    axs[0].set_title("Pred")
-    axs[1].set_title("True")
+            fig_logits, ax_logits = plt.subplots(1, 3)
 
-    def update(i):
-        fig.suptitle(f"Time {i}")
-        axs[0].cla()
-        axs[1].cla()
-        axs[0].imshow(samples[i])
-        axs[1].imshow(a[i])
-    anim = animation.FuncAnimation(fig, update, 20, repeat=True)
-    plt.show()
-    '''
-    i1 = k.Input((None, 8), batch_size=10)
-    if not keras:
-        cell = SimpleRnn(100, activation="tanh")
-        cell1 = SimpleRnn(100, activation="tanh")
-        cell2 = SimpleRnn(10, activation="tanh")
-        output = l.TimeDistributed(l.Dense(8, activation="tanh"))
-        rnn = l.RNN([cell, cell1, cell2], return_sequences=True, stateful=True)
-        o_rnn = rnn(i1)
-        o = output(o_rnn)
-        model = m.Model(i1, o)
-        model.compile("Adam", "mse")
+
+            def update_p(i):
+                fig_logits.suptitle(f"Time {i}")
+                ax_logits[0].cla()
+                ax_logits[1].cla()
+                ax_logits[2].cla()
+                ax_logits[0].imshow(tf.nn.sigmoid(o[i, :, :, 0]).numpy())
+                ax_logits[1].imshow(o[i, :, :, 1].numpy())
+                ax_logits[2].imshow(compute_sigma(o[i, :, :, 2]).numpy())
+                ax_logits[0].set_title("P")
+                ax_logits[1].set_title("Mu")
+                ax_logits[2].set_title("Sigma")
+
+
+            anim_p = animation.FuncAnimation(fig_logits, update_p, o.shape[0] - 2, repeat=True)
+
+            fig, axs = plt.subplots(1, 2)
+            axs[0].set_title("Pred")
+            axs[1].set_title("True")
+
+
+            def update(i):
+                fig.suptitle(f"Time {i}")
+                axs[0].cla()
+                axs[1].cla()
+                axs[0].imshow(samples[i])
+                axs[1].imshow(a[i])
+
+
+            anim = animation.FuncAnimation(fig, update, samples.shape[0] - 2, repeat=True)
+            plt.show()
+        else:
+            cell = models.RecurrentEncoderDecoder2(nodes=N,
+                                                   features=f,
+                                                   channels=5,
+                                                   attention_heads=8,
+                                                   hidden_size=25,
+                                                   dropout=dropout,
+                                                   recurrent_dropout=dropout)
+            rnn = tf.keras.layers.RNN(cell, return_sequences=True, return_state=False, stateful=stateful)
+            outputs = rnn((input_1, input_2))
+            model = tf.keras.models.Model([input_1, input_2], outputs)
+            model.compile(optimizer="rmsprop",
+                          loss=lambda true, pred:
+                          tf.math.reduce_sum(
+                              tf.math.sqrt(
+                                  tf.reduce_mean(
+                                      tf.math.square(true - pred)
+                                  ), axis=(-1, -2)
+                              )
+                          )
+                          )
+            history = model.fit(x=[x[:, :-1, :],a[:, :-1, :]], y=a[:, 1:, :], epochs=250, verbose=1)
+            history = history.history["loss"]
+            samples = tf.squeeze(model([x,a])).numpy()
+            fig, axs = plt.subplots(1,2)
+            def update(i):
+                fig.suptitle(f"Time {i}")
+                axs[0].cla()
+                axs[1].cla()
+                axs[0].imshow(samples[i])
+                axs[1].imshow(a[i])
+
+
+            anim = animation.FuncAnimation(fig, update, samples.shape[0] - 2, repeat=True)
+            plt.show()
     else:
-        cell_k = l.SimpleRNNCell(100, activation="tanh", recurrent_dropout=0.2)
-        cell_k0 = l.SimpleRNNCell(100, activation="tanh", recurrent_dropout=0.2)
-        cell_k1 = l.SimpleRNNCell(10, activation="tanh", recurrent_dropout=0.2)
-        output = l.TimeDistributed(l.Dense(8, activation="tanh"))
-        rnn_k = l.RNN([cell_k, cell_k0, cell_k1], return_sequences=True, stateful=False)(i1)
-        o = output(rnn_k)
-        model = m.Model(i1, o)
-        model.compile("Adam", "mse")
-        # lambda y, y_pred: tf.math.sqrt(tf.reduce_mean(tf.math.square(y - y_pred)), (1,2))
-    xt = []
-    x = np.linspace(0, 5 * np.pi, 100)
-    for i in range(1, 5):
-        x1 = np.cos(x * 0.5 * np.pi / i)[:, None]
-        x2 = np.sin(x * 0.5 * np.pi / i)[:, None]
-        xt.append(x1)
-        xt.append(x2)
-    xt = np.concatenate(xt, axis=-1)
-    x_batched = []
-    for i in range(0, 100, 10):
-        x_batch = xt[i:i + 10]
-        x_batched.append(x_batch)
-    x_batched = np.asarray(x_batched)
-    history = model.fit(x_batched[:, :-1, :], x_batched[:, 1:, :], epochs=300)
-    history = history.history["loss"]
-    plt.plot(history)
-    y = model.predict(x_batched)
-    y = y.reshape(100, 8)
-    fig, axs = plt.subplots(1, 2)
-    axs[0].plot(xt[:, :4])
-    axs[0].set_title("True")
-    axs[1].plot(y[:, :4])
-    axs[1].set_title("Pred")
-    plt.show()
+        if stateful:
+            i1 = k.Input((None, 8), batch_size=10)
+        else:
+            i1 = k.Input((None, 8))
+        if not keras:
+            cell = SimpleRnn(100, activation="tanh", recurrent_dropout=dropout, dropout=dropout)
+            cell1 = SimpleRnn(100, activation="tanh", recurrent_dropout=dropout, dropout=dropout)
+            cell2 = SimpleRnn(10, activation="tanh", recurrent_dropout=dropout, dropout=dropout)
+            output = l.TimeDistributed(l.Dense(8, activation="tanh"))
+            rnn = l.RNN([cell, cell1, cell2], return_sequences=True, stateful=stateful)
+            o_rnn = rnn(i1)
+            o = output(o_rnn)
+            model = m.Model(i1, o)
+            model.compile("Adam", "mse")
+        else:
+            cell_k = l.SimpleRNNCell(100, activation="tanh", recurrent_dropout=dropout, dropout=dropout)
+            cell_k0 = l.SimpleRNNCell(100, activation="tanh", recurrent_dropout=dropout, dropout=dropout)
+            cell_k1 = l.SimpleRNNCell(10, activation="tanh", recurrent_dropout=dropout, dropout=dropout)
+            output = l.TimeDistributed(l.Dense(8, activation="tanh"))
+            rnn_k = l.RNN([cell_k, cell_k0, cell_k1], return_sequences=True, stateful=stateful)(i1)
+            o = output(rnn_k)
+            model = m.Model(i1, o)
+            model.compile("Adam", "mse")
+            # lambda y, y_pred: tf.math.sqrt(tf.reduce_mean(tf.math.square(y - y_pred)), (1,2))
+        xt = []
+        x = np.linspace(0, 5 * np.pi, 100)
+        for i in range(1, 5):
+            x1 = np.cos(x * 0.5 * np.pi / i)[:, None]
+            x2 = np.sin(x * 0.5 * np.pi / i)[:, None]
+            xt.append(x1)
+            xt.append(x2)
+        xt = np.concatenate(xt, axis=-1)
+        x_batched = []
+        for i in range(0, 100, 10):
+            x_batch = xt[i:i + 10]
+            x_batched.append(x_batch)
+        x_batched = np.asarray(x_batched)
+        history = model.fit(x_batched[:, :-1, :], x_batched[:, 1:, :], epochs=300)
+        history = history.history["loss"]
+        plt.plot(history)
+        y = model.predict(x_batched)
+        y = y.reshape(100, 8)
+        fig, axs = plt.subplots(1, 2)
+        axs[0].plot(xt[:, :4])
+        axs[0].set_title("True")
+        axs[1].plot(y[:, :4])
+        axs[1].set_title("Pred")
+        plt.show()
